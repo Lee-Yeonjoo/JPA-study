36d1269ab0b49897dbd0a930a013dd8caee711ab
Yeonjoo Lee <joos0207@naver.com>
2024-05-14 02:49:32 +0900

    순수 jpa repository

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -17,6 +22,26 @@ class MemberJpaRepositoryTest {
 
     @Test
     public void testMember() {
+    }
 
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember = memberJpaRepository.findById(member1.getId()).get();
+        assertThat(findMember).isEqualTo(member1);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(1);
     }
 }
